15c3acb49c4b05d9112f49d5438c37c0cc700039
donghyun-git <donghyun1113@hanmail.net>
2023-08-25 20:56:29 +0900

    Feat: Todo  페이지 Props 관련 Todo 추가

--- src/types.ts
+++ src/types.ts
@@ -3,9 +3,22 @@ export interface AuthForm {
   password: string;
 }
 
+export interface TodoProps {
+  todos: TodoType[];
+}
+
 export type TodoType = {
   id?: number;
   todo: string;
   isCompleted: boolean;
   userId?: number;
 };
+
+export interface TodoTaskProps {
+  todos?: TodoType[];
+  task?: TodoType;
+  // eslint-disable-next-line no-unused-vars
+  handleDeleteTodo: (id: number) => Promise<void>;
+  // eslint-disable-next-line no-unused-vars
+  handleSortedTodo: (todoData: TodoType[], id: number) => void;
+}
